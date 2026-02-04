5459a552356378eb37846399b9f4f141277fbdd2
Ahmed Nagra <ahmednagra244@gmail.com>
2026-02-04 13:08:48 +0500

    Updated base-url #5

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:4000/api/:path*",
+        destination: `${process.env.BASE_URL}/api/:path*`,
       },
     ];
   },
